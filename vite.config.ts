aadae376848a1c95ec9228d93860dde537c5662c
Mateus De Nardo <m.nardo@epilot.cloud>
2022-11-08 22:04:45 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tecpc-solucoes/",
+  base: "/tecpc/",
   plugins: [svgr(), react()],
 });
